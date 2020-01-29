d3299c3115c341c9c6e0879da99ebdfdbc7227a2
Sean-Paul Fenton <sean.fenton@revzilla.com>
2020-01-29 09:11:27 -0500

    Skill Shot - almost done

--- scriptlets/skill_shot.py
+++ scriptlets/skill_shot.py
@@ -1,6 +1,10 @@
 from mpf.core.custom_code import CustomCode
 
 class SkillShot(CustomCode):
+    SHOOTING="shooting"
+    BASKET_MADE="basket_made"
+    ATTEMPTING_SUPER_SKILL_SHOT="attempting_super_skill_shot"
+    SUPER_SKILL_SHOT_AWARDED="super_skill_shot_awarded"
 
     def on_load(self):
         self.reset()
@@ -10,7 +14,6 @@ class SkillShot(CustomCode):
         del kwargs
 
         self.reset()
-        self.choosing = True
 
         self.add_event_handlers()
         self.set_choices()
@@ -19,34 +22,67 @@ class SkillShot(CustomCode):
     def add_event_handlers(self):
         self.machine.events.add_handler('timer_skill_shot_timer_tick', self.on_skill_shot_timer_tick)
         self.machine.events.add_handler('s_skill_shot_basket_active', self.on_skill_shot_basket_active)
-        self.machine.events.add_handler('playfield_active', self.on_playfield_active)
+        self.machine.events.add_handler('cmd_playfield_active', self.on_playfield_active)
         self.machine.events.add_handler('timer_skill_shot_success_timer_complete', self.on_skill_shot_success_timer_complete)
+        self.machine.events.add_handler('timer_skill_shot_deactivation_timer_complete', self.on_skill_shot_deactivation_timer_complete)
+        self.machine.events.add_handler('left_ramp_hit', self.on_super_skill_shot_made)
+        self.machine.events.add_handler('cmd_cancel_skill_shot', self.on_cancel_skill_shot)
+
+        # shp - remove me
+        #self.machine.events.add_handler('s_right_flipper_active', self.on_tester)
+
+ #   def on_tester(self, **kwargs):
+ #       self.machine.events.post('cmd_play_super_skill_shot_success')
+ #       self.machine.events.post('cmd_skill_shot_show_award_extra_ball')
 
     def on_skill_shot_timer_tick(self, **kwargs):
         self.current_choice = list(self.skill_shot_choices)[kwargs['ticks']]
         self.show_slide()
 
     def on_skill_shot_basket_active(self, **kwargs):
-        if self.choosing:
-            self.successful_basket = True
+        if self.is_shooting():
+            self.machine.events.post('cmd_stop_projector_loop')
+            self.state = self.BASKET_MADE
             self.timer.stop()
-            
-        self.choosing = False
+
+    def is_shooting(self):
+        return self.state == self.SHOOTING
+
+    def plunger_basket_made(self):
+        return self.state == self.BASKET_MADE
+
+    def attempting_super_skill_shot(self):
+        return self.state == self.ATTEMPTING_SUPER_SKILL_SHOT
+
+    def on_cancel_skill_shot(self, **kwargs):
+        if self.attempting_super_skill_shot() == True:
+            self.deactivate()
 
     def on_skill_shot_success_timer_complete(self, **kwargs):
         self.deactivate()
 
+    def on_skill_shot_deactivation_timer_complete(self, **kwargs):
+        self.deactivate()
+
     def on_playfield_active(self, **kwargs):
-        self.timer.stop()
+        if self.timer:
+            self.timer.stop()
 
-        if self.successful_basket:
-            self.info_log('shp successful')
-            self.begin_success_timer()
-        else:
-            self.info_log('shp unsuccessful')
-            self.deactivate()
+        if self.state != self.SUPER_SKILL_SHOT_AWARDED:
+            if self.plunger_basket_made():
+                self.state = self.ATTEMPTING_SUPER_SKILL_SHOT
+                self.begin_success_timer()
+            elif self.attempting_super_skill_shot() != True:
+                self.deactivate()
 
-        self.successful_basket: False
+    def on_super_skill_shot_made(self, **kwargs):
+        if self.attempting_super_skill_shot():
+            self.state = self.SUPER_SKILL_SHOT_AWARDED
+            self.success_timer.stop()
+            self.machine.events.post('cmd_play_super_skill_shot_success', award=self.current_choice)
+            self.machine.events.post('cmd_skill_shot_show_award_' + self.current_choice)
+            self.machine.events.post('cmd_super_skill_shot_award_' + self.current_choice)
+            self.begin_deactivation_timer()
 
     def reset(self):
         try:
@@ -55,17 +91,23 @@ class SkillShot(CustomCode):
             self.info_log('noop')
 
         try:
-            self.begin_success_timer.stop()
+            self.success_timer.stop()
         except AttributeError:
             self.info_log('noop')
 
-        self.choosing = False
+        try:
+            self.deactivation_timer.stop()
+        except AttributeError:
+            self.info_log('noop')
+
+        self.state = self.SHOOTING
         self.timer = None
         self.success_timer = None
-        self.successful_basket = False
+        self.deactivation_timer = None
 
     def deactivate(self):
         self.reset()
+        self.machine.events.post('cmd_stop_projector_loop')
         self.machine.events.post('cmd_stop_skill_shot_mode')
 
     def show_slide(self):
@@ -73,16 +115,15 @@ class SkillShot(CustomCode):
 
     def set_choices(self):
         choices = {
-            "big_points": "Big Points",
-            "little_points": "Little Points",
-            "extended_ball_save": "Extended Ball Save",
-            "help": "Award Help",
-            "light_gas_station": "Light Gas Station"
+            "light_lock": "Light Lock",
+            "extra_ball": "Extra Ball", # written
+            "mystery_meat": "Light Mystery Meat",
+            "one_million": "One Million Points", # written
+            "ball_save": "+20 second Ballsave",
+            "add_help": "Add Help Letter", # written
+            "gas_station": "Light Gas Station",
+            "five_million": "Five Million Points", # written
         }
-
-        #"light_extra_ball": "Light Extra Ball",
-        #"light_lock": "Light Lock",
-        #"light_mystery": "Light Mystery"
 
         # Add some conditional choices here
 
@@ -103,13 +144,23 @@ class SkillShot(CustomCode):
         self.success_timer = self.machine.timers.skill_shot_success_timer
 
         self.success_timer.restart_on_complete = False
-        self.success_timer.set_tick_interval(self.tick_interval())
+        self.success_timer.set_tick_interval(1)
         self.success_timer.ticks = 0
         self.success_timer.start_value = 0
-        self.success_timer.end_value = 6
+        self.success_timer.end_value = 10
         self.success_timer.start()
 
+    def begin_deactivation_timer(self):
+        self.deactivation_timer = self.machine.timers.skill_shot_deactivation_timer
+
+        self.deactivation_timer.restart_on_complete = False
+        self.deactivation_timer.set_tick_interval(1)
+        self.deactivation_timer.ticks = 0
+        self.deactivation_timer.start_value = 0
+        self.deactivation_timer.end_value = 2
+        self.deactivation_timer.start()
+
     def tick_interval(self):
-        tick_interval = 0.5
+        tick_interval = 0.3
 
         return tick_interval
